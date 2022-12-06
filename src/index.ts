dc672f0a5e32749f6b69ce99db59b96a72d127b1
Uri Shaked <uri@urishaked.com>
2022-12-06 23:59:55 +0200

    fix: export ELFProgramHeader type

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './elf-enum';
 export * from './elf-header';
 export * from './elf-parser';
 export * from './elf-struct-reader';
+export { ELFProgramHeader } from './elf-program-header';
 export { ELFSymbol } from './elf-symbol';
